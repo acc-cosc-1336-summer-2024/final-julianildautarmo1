91236b0da7f49a5b41c0d3a86ec686683b2c3a58
julianildautarmo1 <julian.ildautarmo@g.austincc.edu>
2024-07-22 23:40:18 +0000

    Question 1 Complete

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,7 @@
-#add import
+from question_a import Stock
+
+Stock_List = [Stock("AAPL", "Apple Inc."),Stock("CAT", "Caterpillar"),Stock("EK", "Eastman Kodak"), Stock("GOOG", "Google"),Stock("MSFT", "Microsoft")]
+
+for Stock in Stock_List:#magic
+    print("Company Name: ", Stock.get_company_name(),"\n")#function that pull private data to public
+    print("Symbol: ", Stock.get_symbol(),"\n-----------------------------")#function that pull private data to public
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,16 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+class Stock:
+    
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol#private
+        
+        self.__company_name = company_name#private
+
+    def get_symbol(self):
+        return self.__symbol#private to public via function
+
+    def get_company_name(self):
+        return self.__company_name#private to public via function
